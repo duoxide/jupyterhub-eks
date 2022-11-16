6220c111e9d8fd4741e0d486083758e355cf2b60
duoxide <zauchka007@gmail.com>
2022-11-16 21:58:37 +0200

    autoscaler+https+github auth

--- kubernetes.tf
+++ kubernetes.tf
@@ -37,20 +37,3 @@ data "kubernetes_service" "lb_dns" {
   }
   depends_on = [helm_release.jupyterhub, module.eks]
 }
-
-/*  resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-example"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "LoadBalancer"
-  }
-} */
--- route53.tf
+++ route53.tf
@@ -2,18 +2,27 @@ data "aws_elb_hosted_zone_id" "main" {}
 
 resource "aws_route53_zone" "aleksejs" {
   name = "dioxide.id.lv"
-  depends_on = [helm_release.jupyterhub]
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.aleksejs.zone_id
-  name    = "jh.dioxide.id.lv"
-  type    = "A"
-  depends_on = [aws_route53_zone.aleksejs]
+  zone_id    = aws_route53_zone.aleksejs.zone_id
+  name       = "jh.dioxide.id.lv"
+  type       = "A"
+  depends_on = [aws_route53_zone.aleksejs, helm_release.jupyterhub]
 
   alias {
     name                   = data.kubernetes_service.lb_dns.status[0].load_balancer[0].ingress[0].hostname
     zone_id                = data.aws_elb_hosted_zone_id.main.id
     evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "dns_challenge" {
+  zone_id    = aws_route53_zone.aleksejs.zone_id
+  name       = "_acme-challenge.jh.dioxide.id.lv"
+  type       = "TXT"
+  ttl        = 300
+  records    = ["uPYDTuKs93ucONhEY_1nPGreMu74ilQn8nhWgSZ_XQY"]
+  depends_on = [aws_route53_zone.aleksejs, helm_release.jupyterhub]
+
 }
